4e1ee83d0b5e33f9f0a48f26533b6ffb1edfa88c
Edgardo Cuello <ecuellog@gmail.com>
2020-07-23 21:52:05 -0700

    Add rich text editor plugin

--- src/views/OrganizationArticleNewView/OrganizationArticleNewView.js
+++ src/views/OrganizationArticleNewView/OrganizationArticleNewView.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, {useState, useRef} from 'react';
+import JoditEditor from "jodit-react";
 
-function OrganizationArticleNewView(props) {
-  return (
-    <div>
-      new article
-    </div>
+const OrganizationArticleNewView = ({}) => {
+	const editor = useRef(null)
+	const [content, setContent] = useState('')
+	
+	const config = {
+    readonly: false,
+    uploader: {
+      insertImageAsBase64URI: true
+    },
+    buttons: "paragraph,|,bold,strikethrough,underline,italic,eraser,|,superscript,subscript,|,ul,ol,|,outdent,indent,|,font,fontsize,brush,|,image,file,video,table,link,|,align,undo,redo,\n,selectall,cut,copy,paste,copyformat,|,hr,symbol,fullsize,print"
+	}
+	
+	return (
+    <JoditEditor
+      ref={editor}
+        value={content}
+        config={config}
+        tabIndex={1} // tabIndex of textarea
+        onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+        onChange={newContent => {}}
+    />
   );
 }
 
-function mapDispatchToProps(dispatch){
-  return {
-  }
-}
-
-function mapStateToProps(state){
-  return {}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(OrganizationArticleNewView);
+export default OrganizationArticleNewView;
