d4aa6114012bc3eeea33552941332fc953f7e97b
Edgardo Cuello <ecuellog@gmail.com>
2020-07-30 21:24:55 -0700

    Add avatar and dropdown menu on navbar

--- src/components/OrganizationNavBar/OrganizationNavBar.js
+++ src/components/OrganizationNavBar/OrganizationNavBar.js
@@ -3,28 +3,64 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import * as _ from 'lodash';
 import fakeLogo from '../../assets/images/fakelogo.png';
+import Avatar from '@material-ui/core/Avatar';
+import { Dropdown } from 'react-bootstrap';
+import * as firebase from "firebase/app";
 import './OrganizationNavBar.scss'
 
 function OrganizationNavBar(props) {
+  function onLogout() {
+    firebase.auth().signOut()
+      .then(() => {
+        console.log('success');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  const AvatarDropdownToggle = React.forwardRef((props, ref) => (
+    <Avatar
+      alt={props.user.displayName}
+      src={props.user.photoURL}
+      className="clickable avatar-colors green"
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        props.onClick(e);
+      }}
+    >E</Avatar>
+  ));
+
   return (
     <nav className="Component_OrganizationNavBar navbar navbar-light fixed-top">
       <img className="logo" src={fakeLogo}></img>
-      <a className="navbar-brand" href="#">hello</a>
+      <Dropdown alignRight>
+        <Dropdown.Toggle
+          as={AvatarDropdownToggle}
+          user={props.user}
+        >
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item as="div">
+            <i className="dropdown-icon far fa-user"></i>
+            <span className="ml-4">Your Profile</span>
+          </Dropdown.Item>  
+          <Dropdown.Divider />
+          <Dropdown.Item as="div" onClick={onLogout}>
+            <i className="dropdown-icon fas fa-sign-out-alt"></i>
+            <span className="ml-4">Logout</span>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </nav>
   );
 }
 
 function mapStateToProps(state){
   return {
-    channels: state.channels.channels,
-    channel: state.channels.activeChannel
+    user: state.auth.user
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    setActiveChannel: (channel) => dispatch(setActiveChannel(channel))
-  }
-}
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(OrganizationNavBar));
+export default withRouter(connect(mapStateToProps, null)(OrganizationNavBar));
