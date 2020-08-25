d47caa4080b462e5737ec913159bbe2822b6feab
Edgardo Cuello <ecuellog@gmail.com>
2020-08-24 18:18:19 -0700

    Change styling of main channel list sidebar

--- src/components/OrganizationChannelSideBar/OrganizationChannelSideBar.js
+++ src/components/OrganizationChannelSideBar/OrganizationChannelSideBar.js
@@ -1,40 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink, useLocation, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { fetchChannels } from '../../store/actions/channels';
-import './OrganizationChannelSideBar.scss';
-import * as _ from 'lodash';
-import OrganizationChannelLink from '../OrganizationChannelLink/OrganizationChannelLink';
-import ModalCreateUpdateChannel from '../ModalCreateUpdateChannel/ModalCreateUpdateChannel';
+import React, { useEffect, useState } from "react";
+import { NavLink, useLocation, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchChannels } from "../../store/actions/channels";
+import "./OrganizationChannelSideBar.scss";
+import * as _ from "lodash";
+import OrganizationChannelLink from "../OrganizationChannelLink/OrganizationChannelLink";
+import ModalCreateUpdateChannel from "../ModalCreateUpdateChannel/ModalCreateUpdateChannel";
 
 function OrganizationChannelSideBar(props) {
   const [showCreateModal, setShowCreateModal] = useState(false);
 
   useEffect(() => {
-    if (props.organization !== null)
-    props.fetchChannels();
+    if (props.organization !== null) props.fetchChannels();
   }, [props.organization]);
 
   return (
-    <>
-      <h4 className="p-2">Channels</h4>
+    <div className="Component_OrganizationChannelSideBar">
+      {_.get(props, "organization.imageSrc") ? (
+        <img className="logo" src={props.organization.imageSrc}></img>
+      ) : (
+        <h2 className="logo mb-0">{_.get(props, "organization.name")}</h2>
+      )}
+      <hr />
       <NavLink
-        className="link"
+        className="link mb-3"
         activeClassName="active"
         to="/channels/all"
-      >All Channels</NavLink>
-      { props.channels.map(channel => (
+      >
+        <i className="sidebar-icon pf-icon-keypad"></i>
+        <span>Todos</span>
+      </NavLink>
+      {props.channels.map(channel => (
         <OrganizationChannelLink key={channel.id} channel={channel} />
       ))}
-      <div className="p-3 clickable" onClick={() => setShowCreateModal(true)}>
-        + Add Channel
+      <div
+        className="btn btn-primary mt-4 w-100"
+        onClick={() => setShowCreateModal(true)}
+      >
+        + Nuevo Canal
       </div>
       <ModalCreateUpdateChannel
         showModal={showCreateModal}
         handleModalClose={() => setShowCreateModal(false)}
         edit={false}
       />
-    </>
+    </div>
   );
 }
 
@@ -42,13 +52,16 @@ function mapStateToProps(state) {
   return {
     organization: state.organizations.activeOrganization,
     channels: state.channels.channels
-  }
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     fetchChannels: () => dispatch(fetchChannels())
-  }
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps) (OrganizationChannelSideBar);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OrganizationChannelSideBar);
--- src/views/OrganizationChannelView/OrganizationChannelView.js
+++ src/views/OrganizationChannelView/OrganizationChannelView.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import WrapperSideBar from '../../components/WrapperSideBar/WrapperSideBar';
-import { setActiveChannel } from '../../store/actions/channels';
-import OrganizationChannelSideBar from '../../components/OrganizationChannelSideBar/OrganizationChannelSideBar';
-import OrganizationNavBar from '../../components/OrganizationNavBar/OrganizationNavBar';
-import * as _ from 'lodash';
-import OrganizationArticleTiles from '../../components/OrganizationArticleTiles/OrganizationArticleTiles';
-import OrganizationEventsTile from '../../components/OrganizationEventsTile/OrganizationEventsTile';
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import WrapperSideBar from "../../components/WrapperSideBar/WrapperSideBar";
+import { setActiveChannel } from "../../store/actions/channels";
+import OrganizationChannelSideBar from "../../components/OrganizationChannelSideBar/OrganizationChannelSideBar";
+import OrganizationNavBar from "../../components/OrganizationNavBar/OrganizationNavBar";
+import * as _ from "lodash";
+import OrganizationArticleTiles from "../../components/OrganizationArticleTiles/OrganizationArticleTiles";
+import OrganizationEventsTile from "../../components/OrganizationEventsTile/OrganizationEventsTile";
 
 function OrganizationChannelView(props) {
   const [channelId, setChannelId] = useState(null);
@@ -15,36 +15,35 @@ function OrganizationChannelView(props) {
   useEffect(() => {
     setChannelId(props.match.params.channelId);
   }, [props.match.params.channelId]);
-  
+
   useEffect(() => {
-    if (channelId !== 'all') {
-      let queryChannel = props.channels.find(channel => channel.id === channelId);
+    if (channelId !== "all") {
+      let queryChannel = props.channels.find(
+        channel => channel.id === channelId
+      );
       props.setActiveChannel(queryChannel);
     }
   }, [props.channels, channelId]);
 
   return (
     <div>
-      <OrganizationNavBar/>
-      <WrapperSideBar sidebar={<OrganizationChannelSideBar/>} navbar={true}>
+      <WrapperSideBar sidebar={<OrganizationChannelSideBar />} navbar={false}>
         <div className="container-fluid p-4">
-          { channelId !== 'all' &&
+          {channelId !== "all" && (
             <>
-              <h2>{_.get(props.channel, 'name')}</h2>
-              <h6>{_.get(props.channel, 'description')}</h6>
+              <h2>{_.get(props.channel, "name")}</h2>
+              <h6>{_.get(props.channel, "description")}</h6>
             </>
-          } 
-          { channelId == 'all' && 
-            <h2>All Channels</h2>
-          }
-          <hr/>
+          )}
+          {channelId == "all" && <h2>All Channels</h2>}
+          <hr />
           <div className="row no-gutters">
             <div className="col-lg-8">
-              <OrganizationArticleTiles channelId={channelId}/>
+              <OrganizationArticleTiles channelId={channelId} />
             </div>
             <div className="col-lg-4 pl-3">
-              <OrganizationEventsTile titleTodoChangeThis='Events'/>
-              <OrganizationEventsTile titleTodoChangeThis='Celebrations'/>
+              <OrganizationEventsTile titleTodoChangeThis="Events" />
+              <OrganizationEventsTile titleTodoChangeThis="Celebrations" />
             </div>
           </div>
         </div>
@@ -53,17 +52,19 @@ function OrganizationChannelView(props) {
   );
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
     channels: state.channels.channels,
     channel: state.channels.activeChannel
-  }
+  };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    setActiveChannel: (channel) => dispatch(setActiveChannel(channel))
-  }
+    setActiveChannel: channel => dispatch(setActiveChannel(channel))
+  };
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(OrganizationChannelView));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(OrganizationChannelView)
+);
