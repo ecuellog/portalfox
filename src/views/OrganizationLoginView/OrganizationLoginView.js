6dd1973032ebde741690b640df94f302430a6322
Edgardo Cuello <ecuellog@gmail.com>
2020-09-07 22:01:28 -0700

    Update styling on login and signup pages

--- src/views/OrganizationLoginView/OrganizationLoginView.js
+++ src/views/OrganizationLoginView/OrganizationLoginView.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import AuthService from '../../services/auth';
 import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, NavLink } from 'react-router-dom';
 import buildUrl from 'build-url';
 import './OrganizationLoginView.scss';
 import googleLogo from '../../assets/images/google/g-logo.png';
@@ -39,41 +39,52 @@ function OrganizationLoginView(props) {
   }
 
   return (
-    <div className="login-register-container d-flex flex-column justify-content-center h-100">
-      <h1 className="text-center mb-5">Log into {_.get(props, 'organization.name')} </h1>
-      <form className="mb-0" onSubmit={onSubmit}>
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={creds.email}
-            onChange={e => setCreds({...creds, email: e.target.value})}
-          >
-          </input>
+    <div className="d-flex flex-column justify-content-center h-100">
+      <img className="login-register-logo" src={_.get(props, 'organization.imageSrc')}></img>
+      <div className="login-register-container">
+        <h2 className="mb-4">Iniciar Sesion </h2>
+        <form className="mb-0" onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={creds.email}
+              onChange={e => setCreds({...creds, email: e.target.value})}
+            >
+            </input>
+          </div>
+          <div className="form-group mb-4">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="form-control"
+              value={creds.password}
+              onChange={e => setCreds({...creds, password: e.target.value})}
+            >
+            </input>
+            <h6 className="text-right mt-1"><NavLink to="/register">¿Olvidaste tu contrasena?</NavLink></h6>
+          </div>
+          <button type="submit" className="btn btn-primary btn-block">Iniciar Sesion</button>
+        </form>
+        <div className="or-separator d-flex justify-content-between align-items-center py-4">
+          <hr className="flex-grow-1 mr-3 ml-4"/>
+          <h6 className="text-center mb-0">o</h6>
+          <hr className="flex-grow-1 ml-3 mr-4"/>          
         </div>
-        <div className="form-group mb-4">
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="form-control"
-            value={creds.password}
-            onChange={e => setCreds({...creds, password: e.target.value})}
-          >
-          </input>
+        <button
+          className="btn btn-google"
+          onClick={redirectToGoogleLogin}
+        >
+          <img className="google-logo" src={googleLogo}></img>
+          <span>Inicia Sesion con Google</span>
+        </button>
+        <div className="text-center mt-5">
+          <h6>¿No tienes cuenta? <NavLink to="/register" className="d-inline p-0">Registrate aqui</NavLink></h6>
         </div>
-        <button type="submit" className="btn btn-primary btn-block">Login</button>
-      </form>
-      <h6 className="py-4 text-center">or</h6>
-      <button
-        className="btn btn-google"
-        onClick={redirectToGoogleLogin}
-      >
-        <img className="google-logo" src={googleLogo}></img>
-        <span>Login with Google</span>
-      </button>
+      </div>
     </div>
   );
 }
--- src/views/OrganizationRegisterView/OrganizationRegisterView.js
+++ src/views/OrganizationRegisterView/OrganizationRegisterView.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useHistory } from "react-router-dom";
+import { useHistory, NavLink } from "react-router-dom";
 import { connect } from 'react-redux';
 import buildUrl from 'build-url';
 import googleLogo from '../../assets/images/google/g-logo.png';
@@ -38,41 +38,51 @@ function OrganizationRegisterView(props) {
   }
 
   return (
-    <div className="login-register-container d-flex flex-column justify-content-center h-100">
-      <h1 className="text-center mb-5">Register at {_.get(props, 'organization.name')} </h1>
-      <form className="mb-0" onSubmit={onSubmit}>
-        <div className="form-group">
-          <input
-            className="form-control"
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={creds.email}
-            onChange={e => setCreds({...creds, email: e.target.value})}
-          >
-          </input>
+    <div className="d-flex flex-column justify-content-center h-100">
+      <img className="login-register-logo" src={_.get(props, 'organization.imageSrc')}></img>
+      <div className="login-register-container">
+        <h2 className="mb-4"> Registrate </h2>
+        <form className="mb-0" onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={creds.email}
+              onChange={e => setCreds({...creds, email: e.target.value})}
+            >
+            </input>
+          </div>
+          <div className="form-group mb-4">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="form-control"
+              value={creds.password}
+              onChange={e => setCreds({...creds, password: e.target.value})}
+            >
+            </input>
+          </div>
+          <button type="submit" className="btn btn-primary btn-block">Registrate</button>
+        </form>
+        <div className="or-separator d-flex justify-content-between align-items-center py-4">
+          <hr className="flex-grow-1 mr-3 ml-4"/>
+          <h6 className="text-center mb-0">o</h6>
+          <hr className="flex-grow-1 ml-3 mr-4"/>
         </div>
-        <div className="form-group mb-4">
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="form-control"
-            value={creds.password}
-            onChange={e => setCreds({...creds, password: e.target.value})}
-          >
-          </input>
+        <button
+          className="btn btn-google"
+          onClick={redirectToGoogleRegister}
+        >
+          <img className="google-logo" src={googleLogo}></img>
+          <span>Register with Google</span>
+        </button>
+        <div className="text-center mt-5">
+          <h6>¿Ya tienes cuenta? <NavLink to="/login" className="d-inline p-0">Inicia Secion</NavLink></h6>
         </div>
-        <button type="submit" className="btn btn-primary btn-block">Register</button>
-      </form>
-      <h6 className="py-4 text-center">or</h6>
-      <button
-        className="btn btn-google"
-        onClick={redirectToGoogleRegister}
-      >
-        <img className="google-logo" src={googleLogo}></img>
-        <span>Register with Google</span>
-      </button>
+      </div>
     </div>
   );
 }
